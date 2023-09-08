f3536f3f5d5ab55b1f9f6478ee46ab55cf0c824a
Cheeduru Vivekvardhan <70307462+VivekCheeduru@users.noreply.github.com>
2023-09-08 07:18:32 +0530

    Update RandomWord.java

--- RandomWord.java
+++ RandomWord.java
@@ -1,12 +1,33 @@
-import java.util.Scanner;
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+
+/** Represents a random word picker */
 public class RandomWord {
-    public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
-        while(scan.hasNext()){
-            String x=scan.next();
-        }
+
+  /**
+   * Reads a sequence of words from standard input and prints one of those words uniformly at random
+   *
+   * @param args sequence of words
+   */
+  public static void main(String[] args) {
+    int index = 0;
+    String champion = "";
+
+    while (!StdIn.isEmpty()) {
+      String word = StdIn.readString();
+      boolean accept = StdRandom.bernoulli(1 / (index + 1.0));
+      if (accept) {
+        champion = word;
+      }
+      index++;
     }
+    StdOut.println(champion);
+  }
 }
